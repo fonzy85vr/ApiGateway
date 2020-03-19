c32f9f4d0d8f5dfde18e2585c06ca44782159551
Marco Andreoli <m.andreoli@magisterinformatica.it>
2020-03-19 18:10:12 +0100

    api adpter for call service

--- config/api-config.js
+++ config/api-config.js
@@ -2,7 +2,12 @@ module.exports = {
     applications: [
         {
             name: 'Holiday',
-            route: '/holiday',
+            route: '/holidays',
+            url: 'http://localhost:3030'
+        },
+        {
+            name: 'HolidayId',
+            route: '/holidays/:id',
             url: 'localhost:3030'
         }
     ]
--- router.js
+++ router.js
@@ -1,11 +1,30 @@
 var express = require('express')
 var router = express.Router();
 var config = require('./config/api-config');
+var axios = require('axios');
+var adapter = require('./apiAdapter');
+
+var api = adapter('http://localhost:3030');
 
 for (let i = 0; i < config.applications.length; i++) {
     var application = config.applications[i];
     router.all(application.route, (req, res, next) => {
-        res.send(req.body);
+        var method = req.method.toLowerCase();
+        var path = req.path;
+        console.log(path);
+        if (method === 'get' || method === 'delete') {
+            api[method](path).then(function (response) {
+                res.send(response.data);
+            }).catch(function (error) {
+                res.send(error);
+            });
+        } else {
+            api[method](path, req.body).then(function (response) {
+                res.send(response.data);
+            }).catch(function (error) {
+                res.send(error.Error);
+            });
+        }
     });
 }
 
