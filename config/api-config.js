79eda40568e001ee3de5963759aceb8e61611b3a
Marco Andreoli <m.andreoli@magisterinformatica.it>
2020-03-20 18:22:40 +0100

    Applications config revisited

--- config/api-config.js
+++ config/api-config.js
@@ -1,39 +1,31 @@
 module.exports = {
     applications: [
         {
-            name: 'Holiday',
-            route: '/holidays',
-            url: 'http://localhost:3030'
-        },
-        {
-            name: 'Hotels',
-            route: '/hotels',
-            url: 'http://localhost:3030'
-        },
-        {
-            name: 'Movements',
-            route: '/movements',
-            url: 'http://localhost:3030'
-        },
-        {
-            name: 'Movements',
-            route: '/movements',
-            url: 'http://localhost:3030'
-        },
-        {
-            name: 'Suitcases',
-            route: '/suitcases',
-            url: 'http://localhost:3030'
-        },
-        {
-            name: 'Tags',
-            route: '/tags',
-            url: 'http://localhost:3030'
-        },
-        {
-            name: 'Items',
-            route: '/items',
-            url: 'http://localhost:3030'
+            name: 'holiday',
+            baseUrl: 'http://192.168.1.170:3030',
+            routes: [
+                {
+                    route: '/holidays'
+                },
+                {
+                    route: '/hotels'
+                },
+                {
+                    route: '/movements'
+                },
+                {
+                    route: '/movements'
+                },
+                {
+                    route: '/suitcases'
+                },
+                {
+                    route: '/tags'
+                },
+                {
+                    route: '/items'
+                }
+            ]
         }
     ]
 }
--- router.js
+++ router.js
@@ -1,50 +1,51 @@
 var express = require('express')
 var router = express.Router();
 var config = require('./config/api-config');
-var axios = require('axios');
 var adapter = require('./apiAdapter');
-
-var api = adapter('http://localhost:3030');
 
 for (let i = 0; i < config.applications.length; i++) {
     var application = config.applications[i];
-    router.all(application.route, (req, res, next) => {
-        var method = req.method.toLowerCase();
-        var path = req.path;
-        console.log(path);
-        if (method === 'get' || method === 'delete') {
-            api[method](path).then(function (response) {
-                res.send(response.data);
-            }).catch(function (error) {
-                res.send(error);
-            });
-        } else {
-            api[method](path, req.body).then(function (response) {
-                res.send(response.data);
-            }).catch(function (error) {
-                res.send(error.Error);
-            });
-        }
-    });
+    var api = adapter(application.baseUrl);
 
-    router.all(application.route + '/:id', (req, res, next) => {
-        var method = req.method.toLowerCase();
-        var path = req.path;
-        console.log(path);
-        if (method === 'get' || method === 'delete') {
-            api[method](path).then(function (response) {
-                res.send(response.data);
-            }).catch(function (error) {
-                res.send(error);
-            });
-        } else {
-            api[method](path, req.body).then(function (response) {
-                res.send(response.data);
-            }).catch(function (error) {
-                res.send(error.Error);
-            });
-        }
-    });
+    for (let r = 0; r < application.routes.length; r++) {
+        router.all(application.routes[r].route, (req, res, next) => {
+            var method = req.method.toLowerCase();
+            var path = req.path;
+            console.log(path);
+            if (method === 'get' || method === 'delete') {
+                api[method](path).then(function (response) {
+                    res.send(response.data);
+                }).catch(function (error) {
+                    res.send(error);
+                });
+            } else {
+                api[method](path, req.body).then(function (response) {
+                    res.send(response.data);
+                }).catch(function (error) {
+                    res.send(error.Error);
+                });
+            }
+        });
+
+        router.all(application.routes[r].route + '/:id', (req, res, next) => {
+            var method = req.method.toLowerCase();
+            var path = req.path;
+            console.log(path);
+            if (method === 'get' || method === 'delete') {
+                api[method](path).then(function (response) {
+                    res.send(response.data);
+                }).catch(function (error) {
+                    res.send(error);
+                });
+            } else {
+                api[method](path, req.body).then(function (response) {
+                    res.send(response.data);
+                }).catch(function (error) {
+                    res.send(error.Error);
+                });
+            }
+        });
+    }
 }
 
 module.exports = router;
